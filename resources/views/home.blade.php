3748a53b06cfabd8bd750829d223666d7c605f5c
mzwae <mrmonjaie@gmail.com>
2020-06-21 21:32:38 +0100

    Add homepage main components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    dashboard
+
+    <div class="lg:flex">
+
+        <div class="lg:w-1/4">
+            @include('_side-links')
+        </div>
+
+        <div class="lg:flex-1">
+
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+
+        </div>
+
+        <div class="lg:w-1/4">
+            @include('_friends-list')
+        </div>
+
+    </div>
+
 @endsection

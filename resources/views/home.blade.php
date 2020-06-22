caa1a2372b9a4c798c435de338b3536f389a02a1
mzwae <mrmonjaie@gmail.com>
2020-06-22 23:41:57 +0100

    Change file name

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="lg:flex">
 
         <div class="lg:w-1/4">
-            @include('_side-links')
+            @include('_sidebar-links')
         </div>
 
         <div class="lg:flex-1">

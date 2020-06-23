9b6f24bdf2fa07b9a45630b77d0de7b5cf74a0d5
mzwae <mrmonjaie@gmail.com>
2020-06-23 16:41:44 +0100

    Update design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,23 @@
 
     <div class="lg:flex">
 
-        <div class="lg:w-1/4">
+        <div class="lg:w-1/6">
             @include('_sidebar-links')
         </div>
 
-        <div class="lg:flex-1">
+        <div class="lg:flex-1 lg:mx-10">
 
-            <div>
+            <div class="border border-blue-400 rounded-lg px-8 py-6">
+                <form action="">
+                    <textarea name="body" class="w-full" placeholder="What's up?"></textarea>
+                </form>
 
+                <hr class="my-4">
+
+                <footer class="flex justify-between">
+                    <img src="https://i.pravatar.cc/40?img=2" alt="img" class="rounded-full mr-2">
+                    <button class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Rant!</button>
+                </footer>
             </div>
 
             <div>
@@ -20,7 +29,7 @@
 
         </div>
 
-        <div class="lg:w-1/4">
+        <div class="lg:w-1/6">
             @include('_friends-list')
         </div>
 

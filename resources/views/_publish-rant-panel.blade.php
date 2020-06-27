0cb596442054a054228f526986332f51a76cbe93
mzwae <mrmonjaie@gmail.com>
2020-06-27 16:09:02 +0100

    Display error msg to user

--- resources/views/_publish-rant-panel.blade.php
+++ resources/views/_publish-rant-panel.blade.php
@@ -1,12 +1,20 @@
 <div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
-    <form action="">
+    <form action="/rants" method="POST">
+
+        @csrf
+
         <textarea name="body" class="w-full" placeholder="What's up?"></textarea>
-    </form>
+   
 
-    <hr class="my-4">
+        <hr class="my-4">
 
-    <footer class="flex justify-between">
-        <img src="https://i.pravatar.cc/40?img=2" alt="img" class="rounded-full mr-2">
-        <button class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Rant!</button>
-    </footer>
+        <footer class="flex justify-between">
+            <img src="https://i.pravatar.cc/40?img=2" alt="img" class="rounded-full mr-2">
+            <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Rant!</button>
+        </footer>
+</form>
+
+@error('body')
+    <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
+@enderror
 </div>

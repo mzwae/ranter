476f286e1e586409ebd8d414ee40bf2e4e9823c6
mzwae <mrmonjaie@gmail.com>
2020-06-27 16:06:08 +0100

    Validate request and persist data

--- app/Http/Controllers/RantController.php
+++ app/Http/Controllers/RantController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class RantController extends Controller
 {
-    //
+    public function store()
+    {
+        // Validate request
+        $attributes = request()->validate(['body' => 'required|max:255']);
+        
+        // Persist data to database
+        Rant::create([
+            'user_id' => auth()->id(),
+            'body' => $attributes['body']
+        ]);  
+        
+        return redirect('/home');
+    }
 }

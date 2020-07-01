4988b3c7b23708658954d9381d727ee65b68d953
mzwae <mrmonjaie@gmail.com>
2020-07-01 14:20:35 +0100

    Update user profile image url

--- resources/views/_publish-rant-panel.blade.php
+++ resources/views/_publish-rant-panel.blade.php
@@ -4,12 +4,12 @@
         @csrf
 
         <textarea name="body" class="w-full" placeholder="What's up?"></textarea>
-   
+
 
         <hr class="my-4">
 
         <footer class="flex justify-between">
-            <img src="https://i.pravatar.cc/40?img=2" alt="img" class="rounded-full mr-2">
+        <img src="https://i.pravatar.cc/40?u={{auth()->user()->email}}" alt="img" class="rounded-full mr-2">
             <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Rant!</button>
         </footer>
 </form>

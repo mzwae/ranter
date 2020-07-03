882c741a98753c1f6580e0cd605dc430e4d92d8b
mzwae <mrmonjaie@gmail.com>
2020-07-03 17:18:29 +0100

    Rename homepage

--- app/Http/Controllers/RantController.php
+++ app/Http/Controllers/RantController.php
@@ -9,7 +9,7 @@ class RantController extends Controller
 
     public function index()
     {
-        return view('home', [
+        return view('rants.index', [
             'rants' => auth()->user()->timeline()
         ]);
     }

cdcba473069f6ac175dcedcc13b482f9975f067e
mzwae <mrmonjaie@gmail.com>
2020-07-03 17:17:51 +0100

    Rename homepage to index

---
+++ resources/views/rants/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="lg:flex lg:justify-between">
+
+        <div class="lg:w-1/6">
+            @include('_sidebar-links')
+        </div>
+
+        <div class="lg:flex-1 lg:mx-10" style="max-width: 700px;">
+
+            @include('_publish-rant-panel')
+
+            <div class="border border-gray-300 rounded-lg">
+
+            @foreach($rants as $rant)
+                @include('rant')
+            @endforeach
+     
+
+            </div>
+
+        </div>
+
+        <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
+            @include('_friends-list')
+        </div>
+
+    </div>
+
+@endsection

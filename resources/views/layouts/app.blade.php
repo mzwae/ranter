2c53e8f7afdaa799a4f872bd33e72afd29026e21
mzwae <mrmonjaie@gmail.com>
2020-07-04 10:02:56 +0100

    Redesign template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,21 +21,37 @@
 </head>
 <body>
     <div id="app">
-        
+
         <section class="px-8 py-4 mb-6">
-            
+
             <header class="continer mx-auto">
                 <h1>The Ranter</h1>
             </header>
-            
+
         </section>
 
         <section class="px-8">
 
             <main class="mx-auto">
-                @yield('content')
+                <div class="lg:flex lg:justify-between">
+
+                    <div class="lg:w-1/6">
+                        @include('_sidebar-links')
+                    </div>
+
+                    <div class="lg:flex-1 lg:mx-10" style="max-width: 700px;">
+
+                        @yield('content')
+
+                    </div>
+
+                    <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
+                        @include('_friends-list')
+                    </div>
+
+                </div>
             </main>
-            
+
         </section>
 
     </div>

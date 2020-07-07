899b46c30160e476ec3dea81db3b01a6d641e5d1
mzwae <mrmonjaie@gmail.com>
2020-07-07 23:48:43 +0100

    Update profile page design

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,7 +2,24 @@
 
 @section('content')
 
-    <h3>Profile page for {{ $user->name }}</h3>
+   <header class="mb-6">
+       <img src="/images/default-profile-banner.jpg" alt="profile-banner">
+
+       <div class="flex justify-between">
+
+           <div>
+                <h2>{{ $user->name }}</h2>
+                <p>Joined {{ $user->created_at->diffForHumans()}}</p>
+           </div>
+
+           <div>
+               <a href="" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Edit Profile</a>
+               <a href="" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Follow Me</a>
+           </div>
+
+       </div>
+
+   </header>
 
     <hr>
 

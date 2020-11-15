c57359a07eca2a0bffac5c21ab2b0c1829ecca59
mzwae <mrmonjaie@gmail.com>
2020-11-15 11:14:36 +0000

    Display sidebards to authenticated users only

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,9 +35,11 @@
             <main class="mx-auto">
                 <div class="lg:flex lg:justify-between">
 
-                    <div class="lg:w-1/6">
-                        @include('_sidebar-links')
-                    </div>
+                   @if (auth()->check())
+                        <div class="lg:w-1/6">
+                            @include('_sidebar-links')
+                        </div>
+                   @endif
 
                     <div class="lg:flex-1 lg:mx-10" style="max-width: 700px;">
 
@@ -45,9 +47,11 @@
 
                     </div>
 
-                    <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
-                        @include('_friends-list')
-                    </div>
+                   @if (auth()->check())
+                        <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
+                            @include('_friends-list')
+                        </div>
+                   @endif
 
                 </div>
             </main>

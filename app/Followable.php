89039001c53883a88b5785672f2a733ea0d0e5c4
mzwae <mrmonjaie@gmail.com>
2020-11-17 20:03:48 +0000

    Add followable trait

---
+++ app/Followable.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+trait Followable
+{
+    public function follows()
+    {
+        return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
+    }
+
+    public function follow(User $user)
+    {
+        return $this->follows()->save($user);
+    }
+
+    public function following()
+    {
+
+    }
+
+
+}

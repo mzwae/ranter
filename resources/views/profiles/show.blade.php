2353ca37ac79a3951a623ed74e7197087668bceb
mzwae <mrmonjaie@gmail.com>
2020-11-19 18:13:10 +0000

    Change follow me button to a form to use post method

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -12,9 +12,17 @@
                 <p class="text-sm">Joined {{ $user->created_at->diffForHumans()}}</p>
            </div>
 
-           <div>
-               <a href="" class="bg-blue-500 rounded-lg shadow py-2 px-4 text-black text-xs mr-2">Edit Profile</a>
-               <a href="" class="bg-blue-500 rounded-lg shadow py-2 px-4 text-white text-xs">Follow Me</a>
+           <div class="flex">
+               <a href="" class="bg-blue-500 rounded-full shadow py-2 px-4 text-black text-xs mr-2">Edit Profile</a>
+               {{-- <a href="" class="bg-blue-500 rounded-lg shadow py-2 px-4 text-white text-xs">Follow Me</a> --}}
+
+           <form method="POST" action="/profiles/{{ $user->name }}/follow">
+                @csrf
+
+                <button type="submit" class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs">
+                    Follow Me
+                </button>
+           </form>
            </div>
 
        </div>

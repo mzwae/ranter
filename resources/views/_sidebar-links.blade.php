61fcaf8d5b6dd2f2235777e1b3e61baa986bb1f7
mzwae <mrmonjaie@gmail.com>
2020-11-20 11:04:27 +0000

    Add auth user profile url link

--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -53,7 +53,7 @@
         <li>
             <a
                 class="font-bold text-lg mb-4 block"
-                href=""
+                href="{{ route('profile', auth()->user()->name) }}"
             >
                 profile
             </a>

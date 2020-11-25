9b4341d6d335b8bc5682e8886199fa53200f5416
mzwae <mrmonjaie@gmail.com>
2020-11-25 19:14:14 +0000

    Comment out alternative code

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::middleware('auth')->group(function(){
     Route::post('/rants', 'RantController@store');
     Route::post('/profiles/{user:name}/follow', 'FollowsController@store');
     Route::get('/profiles/{user:name}/edit', 'ProfilesController@edit');
+    // Route::get('/profiles/{user:name}/edit', 'ProfilesController@edit')->middleware('can:edit,user');
 });
 
 Route::get('/profiles/{user:name}', 'ProfilesController@show')->name('profile');

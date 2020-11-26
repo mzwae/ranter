f9ef773173739608f5d5d0dba1417971fe4196ad
mzwae <mrmonjaie@gmail.com>
2020-11-26 19:51:03 +0000

    Add log fascade

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ProfilesController extends Controller
 {
@@ -19,6 +20,8 @@ class ProfilesController extends Controller
         // }
 
         $this->authorize('edit', $user);
+
+        // Log::warning($user);
 
         return view('profiles.edit', compact('user'));
     }

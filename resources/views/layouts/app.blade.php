88c59da6d8757a5b1268faa56df96c8bf00770c3
mzwae <mrmonjaie@gmail.com>
2020-11-29 19:55:09 +0000

    Use Bootstrap to update the design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,41 +23,36 @@
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
+    <div id="app" class="container">
 
-        <section class="px-8 py-4 mb-6">
+        <div class="row">
 
-            <header class="continer mx-auto">
+            <header class="">
                 <h1>The Ranter</h1>
             </header>
 
-        </section>
+        </div>
 
-        <section class="px-8">
+        <div class="row">
 
-            <main class="mx-auto">
-                <div class="lg:flex lg:justify-between">
-                    @auth('')
-                    <div class="lg:w-1/6">
-                        @include('_sidebar-links')
-                    </div>
-                    @endauth
-                    <div class="lg:flex-1 lg:mx-10" style="max-width: 700px;">
+            @auth('')
+            <div class="col-md-2">
+                @include('_sidebar-links')
+            </div>
+            @endauth
+            <div class="col-md-8" style="max-width: 700px;">
 
-                        @yield('content')
+                @yield('content')
 
-                    </div>
+            </div>
 
-                  @auth('')
-                    <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
-                        @include('_friends-list')
-                    </div>
-                  @endauth
+            @auth('')
+            <div class="col-md-2">
+                @include('_friends-list')
+            </div>
+            @endauth
 
-                </div>
-            </main>
-
-        </section>
+        </div>
 
     </div>
 </body>

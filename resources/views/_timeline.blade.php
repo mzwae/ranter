194063da4654491ba1b57c983ac128529415f4e7
mzwae <mrmonjaie@gmail.com>
2020-11-29 19:37:10 +0000

    Display no rants yet if user has no rants

--- resources/views/_timeline.blade.php
+++ resources/views/_timeline.blade.php
@@ -1,7 +1,12 @@
 <div class="border border-gray-300 rounded-lg">
 
-    @foreach($rants as $rant)
+    @forelse($rants as $rant)
         @include('rant')
-    @endforeach
+
+        @empty
+        <p class="p-4">No rants yet.</p>
+
+
+    @endforelse
 
 </div>

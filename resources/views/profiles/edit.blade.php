9f0c842210d5b349244488692eba00341fc89695
mzwae <mrmonjaie@gmail.com>
2020-12-01 19:41:44 +0000

    Add edit profile form

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -2,6 +2,34 @@
 
 @section('content')
 
-   Edit profile
+<h1 class="display-3">Edit your profile</h1>
+
+<form action="{{ $user->path() }}" method="post">
+    @csrf
+    @method('patch')
+
+    <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text"
+          class="form-control" name="name" value="{{ $user->name }}">
+
+          @error('name')
+            <p class="text-danger">{{ $message }}</p>
+          @enderror
+    </div>
+
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input type="email"
+        class="form-control" name="email" value="{{ $user->email }}">
+
+        @error('email')
+            <p class="text-danger">{{ $message }}</p>
+        @enderror
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+
+   </form>
 
 @endsection

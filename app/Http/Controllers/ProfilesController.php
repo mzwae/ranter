9d51e1265060161245fa504eeb3d36728e9db1fc
mzwae <mrmonjaie@gmail.com>
2020-12-04 19:43:01 +0000

    Add update user profile method

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Log;
 
 class ProfilesController extends Controller
@@ -24,5 +25,18 @@ class ProfilesController extends Controller
         // Log::warning($user);
 
         return view('profiles.edit', compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $attributes = request()->validate([
+            'username' => ['string', 'required', 'max:255', 'alpha_dash', Rule::unique('users')->ignore($user)],
+            'name' => ['string', 'required', 'max:255'],
+            'email' => ['string', 'required', 'email', 'max:255']
+        ]);
+
+        $user->update($attributes);
+
+        return redirect($user->path());
     }
 }

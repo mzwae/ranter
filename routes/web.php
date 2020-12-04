d18e43eec7ee94c3a330144276ff8ce6f7cdb6b2
mzwae <mrmonjaie@gmail.com>
2020-12-04 19:41:32 +0000

    Update user profile patch  route parameter to username instead of name

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::middleware('auth')->group(function(){
     Route::post('/profiles/{user:username}/follow', 'FollowsController@store');
     Route::get('/profiles/{user:username}/edit', 'ProfilesController@edit');
     // Route::get('/profiles/{user:name}/edit', 'ProfilesController@edit')->middleware('can:edit,user');
-    Route::patch('/profiles/{user:name}', 'ProfilesController@update');
+    Route::patch('/profiles/{user:username}', 'ProfilesController@update');
 });
 
 Route::get('/profiles/{user:username}', 'ProfilesController@show')->name('profile');

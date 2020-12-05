5a0c0c6d73ab94f60a5b3fa398ffbea36be0e26e
mzwae <mrmonjaie@gmail.com>
2020-12-05 20:35:09 +0000

    Refactor Code: use toggle method

--- app/Followable.php
+++ app/Followable.php
@@ -28,11 +28,7 @@ trait Followable
 
     public function toggleFollow(User $user)
     {
-        if ($this->following($user)) {
-            return $this->unfollow($user);
-        }
-
-        return $this->follow($user);
+      $this->follows()->toggle($user);
     }
 
 

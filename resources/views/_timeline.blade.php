bcd1cb5d4fb4ab7065512e1b1f14cc994a971951
mzwae <mrmonjaie@gmail.com>
2020-12-09 20:53:02 +0000

    Add rants pagination to timeline view

--- resources/views/_timeline.blade.php
+++ resources/views/_timeline.blade.php
@@ -3,10 +3,14 @@
     @forelse($rants as $rant)
         @include('rant')
 
+
         @empty
         <p class="p-4">No rants yet.</p>
 
 
-    @endforelse
 
+    @endforelse
+</div>
+<div class="flex justify-between w-64 mx-auto mt-5 mb-5">
+    {{ $rants->links() }}
 </div>

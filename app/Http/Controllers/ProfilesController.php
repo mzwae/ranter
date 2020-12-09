598192244e767b98007c7912c5f349e41563de6b
mzwae <mrmonjaie@gmail.com>
2020-12-09 20:49:13 +0000

    Add rants pagination to user profile page

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -11,7 +11,10 @@ class ProfilesController extends Controller
 {
     public function show(User $user)
     {
-        return view('profiles.show', compact('user'));
+        return view('profiles.show', [
+            'user' => $user,
+            'rants' => $user->rants()->paginate(5)
+        ]);
     }
 
     public function edit(User $user)

c1d3ce0dd63ad68a8521371fa469abdb04d6bae8
mzwae <mrmonjaie@gmail.com>
2020-12-20 19:35:15 +0000

    Add more spacing between icon and number in like/dislike buttons

--- resources/views/components/like-buttons.blade.php
+++ resources/views/components/like-buttons.blade.php
@@ -2,12 +2,12 @@
     {{-- <form action="/rants/{{ $rant->id }}/like" method="post"> --}}
     <form action="{{ action('RantLikesController@store', ['rant' => $rant->id]) }}" method="post">
         @csrf
-        <button type="submit" class="fa fa-thumbs-up p-2 btn btn-outline-success mr-5 {{ $rant->isLikedBy(current_user()) ? 'disabled':'' }}">{{ $rant->likes ?: 0 }}</button>
+        <button type="submit" class="fa fa-thumbs-up p-2 btn btn-outline-success mr-5 {{ $rant->isLikedBy(current_user()) ? 'disabled':'' }}"> {{ $rant->likes ?: 0 }}</button>
     </form>
 
     <form action="/rants/{{ $rant->id }}/like" method="post">
         @csrf
         @method('delete')
-        <button class="fa fa-thumbs-down p-2 btn btn-outline-danger {{ $rant->isDislikedBy(current_user()) ? 'disabled':'' }}">{{ $rant->dislikes ?: 0}}</button>
+        <button class="fa fa-thumbs-down p-2 btn btn-outline-danger {{ $rant->isDislikedBy(current_user()) ? 'disabled':'' }}"> {{ $rant->dislikes ?: 0}}</button>
     </form>
 </div>

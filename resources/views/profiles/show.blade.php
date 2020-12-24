e8209dc9e4ac006ff97f02a3cd97d107326b7d5c
mzwae <mrmonjaie@gmail.com>
2020-12-24 19:13:26 +0000

    Make user profile bio section dynamic in show profile view

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -24,9 +24,8 @@
     </div>
 
     <p class="text-sm">
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-        The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here,
-        content here', making it look like readable English.
+        {{ $user->bio ? $user->bio->body : "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium" }}
+
     </p>
 
     <img src="{{ $user->avatar }}" alt="avatar" class="rounded-full mr-2 absolute" style="width: 150px; left: calc(50% - 75px); top: 38%;">

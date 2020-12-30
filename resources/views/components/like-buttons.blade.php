c3272d2eb0cd103552692f2c16d5a3e8b38677a4
mzwae <mrmonjaie@gmail.com>
2020-12-30 19:22:31 +0000

    Add delete rant button

--- resources/views/components/like-buttons.blade.php
+++ resources/views/components/like-buttons.blade.php
@@ -12,6 +12,7 @@
     </form>
 
     @if(current_user()->id === $rant->user_id)
-        <a class="fa fa-pencil p-2 ml-5 btn btn-outline-info" data-toggle="modal" data-target="#editModal-{{ $rant->id }}">Edit</a>
+        <a class="fa fa-pencil p-2 ml-5 btn btn-outline-info" data-toggle="modal" data-target="#editModal-{{ $rant->id }}"> Edit</a>
+        <a class="fa fa-trash p-2 ml-5 btn btn-outline-danger" data-toggle="modal" data-target="#deleteModal-{{ $rant->id }}"> Delete</a>
     @endif
 </div>
